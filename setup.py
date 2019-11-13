f7c2a11e0042a5eadd575c816cf5410325008bac
fredericvl <frederic.van.linthoudt@gmail.com>
2019-11-13 11:18:38 +0100

    Reorganize repo

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="efestoclient",
+    version="0.0.8",
+    author="Frederic Van Linthoudt",
+    author_email="frederic.van.linthoudt@gmail.com",
+    description="EfestoClient provides controlling Efesto heat devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fredericvl/efestoclient",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
